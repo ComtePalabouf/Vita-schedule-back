fe3c986bd1ea87dc157ef4df99c599b17dd0e474
Cyril Bernadet <cyril.bernadet@gmail.com>
2022-06-04 16:42:26 +0200

    Add global CORS config and opponents get method

--- src/main/java/com/kinikun/vitalityschedule/VitalityScheduleApplication.java
+++ src/main/java/com/kinikun/vitalityschedule/VitalityScheduleApplication.java
@@ -1,13 +1,28 @@
 package com.kinikun.vitalityschedule;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class VitalityScheduleApplication {
+    @Value("${CORS_URL}")
+    private String corsOrigins;
 
 	public static void main(String[] args) {
 		SpringApplication.run(VitalityScheduleApplication.class, args);
 	}
-
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/api/**").allowedOrigins(corsOrigins);
+			}
+		};
+	}
 }
--- src/main/java/com/kinikun/vitalityschedule/controller/MatchesController.java
+++ src/main/java/com/kinikun/vitalityschedule/controller/MatchesController.java
@@ -2,11 +2,12 @@ package com.kinikun.vitalityschedule.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpClientErrorException.NotFound;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
@@ -16,7 +17,6 @@ public class MatchesController {
     @Value("${API_TOKEN}")
     private String apiToken;
     
-    @CrossOrigin(origins = "https://vitality-schedule.ddns.net/")
     @GetMapping()
     public ResponseEntity<String> getMatches(@RequestParam String[] teamIds) {
         RestTemplate restTemplate = new RestTemplate();
@@ -27,5 +27,21 @@ public class MatchesController {
         apiUrl.append(apiToken);
 
         return restTemplate.getForEntity(apiUrl.toString(), String.class);
+    }
+    
+    @GetMapping("/{matchId}/opponents")
+    public ResponseEntity<String> getMatchOpponents(@PathVariable String matchId) {
+        try {
+            RestTemplate restTemplate = new RestTemplate();
+
+            StringBuilder apiUrl = new StringBuilder("https://api.pandascore.co/matches/");
+            apiUrl.append(matchId);
+            apiUrl.append("/opponents?token=");
+            apiUrl.append(apiToken);
+
+            return restTemplate.getForEntity(apiUrl.toString(), String.class);
+        } catch (NotFound exception) {
+            return null;
+        }
     }
 }
