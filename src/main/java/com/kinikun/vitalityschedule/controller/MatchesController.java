1d5615b5969a81eccb54b680979747f002c1ba93
Cyril Bernadet <cyril.bernadet@gmail.com>
2022-07-23 11:34:45 +0200

    Add a start limit for matches

--- src/main/java/com/kinikun/vitalityschedule/controller/MatchesController.java
+++ src/main/java/com/kinikun/vitalityschedule/controller/MatchesController.java
@@ -1,5 +1,9 @@
 package com.kinikun.vitalityschedule.controller;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,8 +25,15 @@ public class MatchesController {
     public ResponseEntity<String> getMatches(@RequestParam String[] teamIds) {
         RestTemplate restTemplate = new RestTemplate();
 
+        LocalDateTime startFilterDate = LocalDateTime.now().minus(1, ChronoUnit.MONTHS);
+        LocalDateTime endFilterDate = LocalDateTime.now().plus(1, ChronoUnit.YEARS);
+
         StringBuilder apiUrl = new StringBuilder("https://api.pandascore.co/matches?per_page=100&filter[opponent_id]=");
         apiUrl.append(String.join(",", teamIds));
+        apiUrl.append("&range[begin_at]=");
+        apiUrl.append(startFilterDate.format(DateTimeFormatter.ISO_LOCAL_DATE));
+        apiUrl.append(",");
+        apiUrl.append(endFilterDate.format(DateTimeFormatter.ISO_LOCAL_DATE));
         apiUrl.append("&token=");
         apiUrl.append(apiToken);
 
